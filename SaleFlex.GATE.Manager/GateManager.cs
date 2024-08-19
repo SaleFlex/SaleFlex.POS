5abf20188695799a57210f21657505b9663a6a7a
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-08-19 05:33:42 +0300

    SaleFlex.GATE.Manager class library added

---
+++ SaleFlex.GATE.Manager/GateManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleFlex.GATE.Manager
+{
+    public class GateManager
+    {
+    }
+}
