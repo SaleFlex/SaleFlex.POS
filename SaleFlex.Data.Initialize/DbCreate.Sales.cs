1dcd21ae8df1224020a5d81e691ab862ee2ce0da
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-09-04 00:36:46 +0300

    bCreateTableTransactionHead method added.

--- SaleFlex.Data.Initialize/DbCreate.Sales.cs
+++ SaleFlex.Data.Initialize/DbCreate.Sales.cs
@@ -5,6 +5,7 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SaleFlex.CommonLibrary;
 
 namespace SaleFlex.Data.Initialize
 {
@@ -19,6 +20,7 @@ namespace SaleFlex.Data.Initialize
 
             var DbTableCreateMethods = new List<Func<bool>>
             {
+                bCreateTableTransactionHead
             };
 
             foreach (var DbTableCreateMethod in DbTableCreateMethods)
@@ -28,6 +30,59 @@ namespace SaleFlex.Data.Initialize
                     bReturnValue = false;
                     break;
                 }
+            }
+
+            return bReturnValue;
+        }
+
+        private static bool bCreateTableTransactionHead()
+        {
+            bool bReturnValue = false;
+            try
+            {
+                string strCreateTableQuery =
+                    @"CREATE TABLE TableTransactionHead (
+                        Id                           INTEGER  PRIMARY KEY ASC AUTOINCREMENT
+                                                              UNIQUE
+                                                              NOT NULL,
+                        FkCashierId                  INTEGER  NOT NULL,
+                        TransactionDateTime          DATETIME NOT NULL,
+                        FkTransactionDocumentTypeId  INTEGER  NOT NULL,
+                        FkCustomerId                 INTEGER,
+                        TransactionNo                TEXT     NOT NULL,
+                        ReceiptNumber                INTEGER  NOT NULL,
+                        ZNumber                      INTEGER  NOT NULL,
+                        ReceiptTotalPrice            REAL,
+                        ReceiptTotalVat              REAL,
+                        TotalDiscountAmount          REAL,
+                        TransactionsDiscountAmount   REAL,
+                        CustomerTotalDiscountAmount  REAL,
+                        PromotionTotalDiscountAmount REAL,
+                        SurchargeAmount              REAL,
+                        PaymentAmount                REAL,
+                        ChangeAmount                 REAL,
+                        RoundAmount                  REAL,
+                        IsVoided                     BOOLEAN  NOT NULL
+                                                              DEFAULT (0) 
+                    );";
+
+
+                using (SQLiteConnection xSQLiteConnection = new SQLiteConnection(strCreateConnectionString(CommonProperty.prop_strDatabasePosFileName)))
+                {
+                    using (SQLiteCommand xSQLiteCommand = new System.Data.SQLite.SQLiteCommand(xSQLiteConnection))
+                    {
+                        xSQLiteConnection.Open();                           // Open the connection to the database
+
+                        xSQLiteCommand.CommandText = strCreateTableQuery;   // Set CommandText to our query that will create the table
+                        int iResult = xSQLiteCommand.ExecuteNonQuery();     // Execute the create table query
+
+                        xSQLiteConnection.Close();        // Close the connection to the database
+                    }
+                }
+            }
+            catch (Exception xException)
+            {
+                xException.strTraceError();
             }
 
             return bReturnValue;
