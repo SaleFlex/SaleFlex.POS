f53c2a9aba33d4e85cb12bbbea2e8ce93719852c
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-09-13 23:38:03 +0300

    bClosure method added to PosManager class.

---
+++ SaleFlex.POS.Manager/PosManager.Closure.cs
@@ -0,0 +1,19 @@
+﻿using SaleFlex.Data.AccessLayer;
+using SaleFlex.Data.Models;
+using SaleFlex.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleFlex.POS.Manager
+{
+    public partial class PosManager
+    {
+        public bool bClosure(EnumPaymentType prm_enumType, long prm_lPaymentAmount)
+        {
+            return true;
+        }
+    }
+}
