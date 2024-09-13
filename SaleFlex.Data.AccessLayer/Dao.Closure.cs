97bebbbb616d31d8edfec3bdcf207b0b9df3bdea
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-09-13 15:13:19 +0300

    bInsertClosure method finished.

--- SaleFlex.Data.AccessLayer/Dao.Closure.cs
+++ SaleFlex.Data.AccessLayer/Dao.Closure.cs
@@ -1,26 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
 using SaleFlex.CommonLibrary;
 using SaleFlex.Data.Models;
+using SaleFlex.Data.SQLite;
 
 namespace SaleFlex.Data.AccessLayer
 {
     public partial class Dao
     {
-        public bool bSaveClosureParameters(ClosureDataModel prm_xEndOfDayDataModel)
+
+
+        public bool bInsertClosure(ref ClosureDataModel prm_xClosureDataModel)
         {
+            bool bReturnValue = false;
             try
             {
-                return true;
+                var query = string.Format("INSERT INTO TableClosure(ZNumber, ReceiptNumber, ClosureDateTime, FkPosId, FkCashierId, TotalTransactionCount, TotalTransactionAmount, TotalVatAmount, TotalDiscountCount, TotalDiscountAmount, TotalSurchargeCount, TotalSurchargeAmount, TotalChangeCount, TotalChangeAmount, TotalRoundAmount, CumulativeTotalAmount, CumulativeVatAmount, Canceled, Deleted) VALUES({0},{1},datetime('now', 'localtime'),{1},{2},'{3}',{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},0,0); SELECT last_insert_rowid() AS InsertedId",
+                    prm_xClosureDataModel.iReceiptNumber,
+                    prm_xClosureDataModel.iZNumber,
+                    prm_xClosureDataModel.lFkPosId,
+                    prm_xClosureDataModel.lFkCashierId,
+                    prm_xClosureDataModel.iTotalTransactionCount,
+                    prm_xClosureDataModel.lTotalTransactionAmount,
+                    prm_xClosureDataModel.lTotalDiscountAmount,
+                    prm_xClosureDataModel.lTotalVatAmount,
+                    prm_xClosureDataModel.iTotalDiscountCount,
+                    prm_xClosureDataModel.lTotalDiscountAmount,
+                    prm_xClosureDataModel.iTotalSurchargeCount,
+                    prm_xClosureDataModel.lTotalSurchargeAmount,
+                    prm_xClosureDataModel.iTotalChangeCount,
+                    prm_xClosureDataModel.lTotalChangeAmount,
+                    prm_xClosureDataModel.lTotalRoundAmount,
+                    prm_xClosureDataModel.lCumulativeTotalAmount,
+                    prm_xClosureDataModel.lCumulativeVatAmount);
+                DataTable xDataTable = Dbo.xGetInstance(CommonProperty.prop_strDatabaseSalesFileNameAndPath).xExecuteDataTable(query);
+
+                if (int.Parse(xDataTable.Rows[0]["InsertedId"].ToString()) > 0)
+                {
+                    bReturnValue = true;
+                    prm_xClosureDataModel.lId = int.Parse(xDataTable.Rows[0]["InsertedId"].ToString());
+                }
             }
             catch (Exception xException)
             {
                 xException.strTraceError();
-                return false;
             }
+            return bReturnValue;
         }
     }
 }
