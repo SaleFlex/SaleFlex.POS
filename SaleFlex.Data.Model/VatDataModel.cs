46cf203838984797cdfccd305235a40dfee8403e
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-09-17 01:15:37 +0300

    Comments added to VatDataModel

--- SaleFlex.Data.Model/VatDataModel.cs
+++ SaleFlex.Data.Model/VatDataModel.cs
@@ -5,12 +5,23 @@ using System.Text;
 
 namespace SaleFlex.Data.Models
 {
+    // This class represents the VAT (Value Added Tax) data model used in the SaleFlex system.
+    // It contains information about different VAT rates and their details.
     public class VatDataModel
     {
+        // Unique identifier for the VAT record.
         public int iId { get; set; }
+
+        // VAT number or code to identify the specific VAT category.
         public int iNo { get; set; }
+
+        // Name of the VAT type/category (e.g., "Standard VAT", "Reduced VAT").
         public string strName { get; set; }
+
+        // The rate of the VAT, represented as an integer (e.g., 18 for 18% VAT rate).
         public int iRate { get; set; }
+
+        // Additional description or details about the VAT.
         public string strDescription { get; set; }
     }
 }
