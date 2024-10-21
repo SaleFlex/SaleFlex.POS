7abf758382060af67c6b0e865729766db2693979
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-10-21 23:07:24 +0300

    WarehouseDataModel added.

--- SaleFlex.Data.Model/WarehouseDataModel.cs
+++ SaleFlex.Data.Model/WarehouseDataModel.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace SaleFlex.Data.Models
 {
+    // Basic Warehouse Model: Defines each warehouse.
     internal class WarehouseDataModel
     {
+        public WarehouseDataModel()
+        {
+            xListWarehouseProductDataModel = new List<WarehouseProductDataModel>();
+        }
+
+        // Warehouse Id
+        public int iId { get; set; }
+
+        // Warehouse Name
+        public string strName { get; set; }
+
+        // Warehouse Location
+        public string strLocation { get; set; }
+
+        // Warehouse Type (Main Warehouse, Branch Warehouse, etc.)
+        public string strType { get; set; }
+
+        // List of products in the warehouse
+        public List<WarehouseProductDataModel> xListWarehouseProductDataModel { get; set; }
     }
 }
