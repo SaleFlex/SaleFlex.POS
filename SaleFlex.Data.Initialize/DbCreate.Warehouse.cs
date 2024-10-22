14976aad7ca3a7bb023d22549361121d8ad70837
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-10-22 21:19:13 +0300

    TableWarehouseTransaction DB table create query added.

--- SaleFlex.Data.Initialize/DbCreate.Warehouse.cs
+++ SaleFlex.Data.Initialize/DbCreate.Warehouse.cs
@@ -5,6 +5,7 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SaleFlex.CommonLibrary;
 
 namespace SaleFlex.Data.Initialize
 {
@@ -28,6 +29,49 @@ namespace SaleFlex.Data.Initialize
                     bReturnValue = false;
                     break;
                 }
+            }
+
+            return bReturnValue;
+        }
+
+        private static bool bCreateTableWarehouseTransaction()
+        {
+            bool bReturnValue = false;
+            try
+            {
+                string strCreateTableQuery =
+                    @"CREATE TABLE If Not Exists TableWarehouseTransaction (
+                        Id                     INTEGER PRIMARY KEY
+                                                       UNIQUE
+                                                       NOT NULL,
+                        FkWarehouseId          INTEGER,
+                        FkProductId            INTEGER,
+                        TransactionType        TEXT,
+                        Quantity               INTEGER,
+                        TransactionDate        DATETIME,
+                        Description            TEXT
+                    );";
+
+
+                using (SQLiteConnection xSQLiteConnection = new SQLiteConnection(strCreateConnectionString(CommonProperty.prop_strDatabaseWarehouseFileNameAndPath)))
+                {
+                    using (SQLiteCommand xSQLiteCommand = new System.Data.SQLite.SQLiteCommand(xSQLiteConnection))
+                    {
+                        xSQLiteConnection.Open();                           // Open the connection to the database
+
+                        xSQLiteCommand.CommandText = strCreateTableQuery;   // Set CommandText to our query that will create the table
+                        int iResult = xSQLiteCommand.ExecuteNonQuery();     // Execute the create table query
+
+                        if (iResult >= 0)
+                            bReturnValue = true;
+
+                        xSQLiteConnection.Close();        // Close the connection to the database
+                    }
+                }
+            }
+            catch (Exception xException)
+            {
+                xException.strTraceError();
             }
 
             return bReturnValue;
